107f7f4700089ab02ff71e6e6729df9a3f81fcfb
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-11-20 11:38:43 +0530

    compose deploy, jwtmiddleware

--- up.sh
+++ up.sh
@@ -13,7 +13,9 @@ echo issuebase-server-db-1 "$mysql_ip"
 
 echo issuebase-server-redis-1 "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' issuebase-server-db-1)"
 
-#docker exec -it "$(docker container ls | grep 'issuebase-server_tomcat_1' | awk '{print $1}')" sh -c "mv webapps webapps2 && mv webapps.dist/ webapps"
-#docker exec -it "$(docker container ls | grep 'issuebase-server_tomcat_1' | awk '{print $1}')" sh -c "rm webapps/manager/META-INF/context.xml && mv context.xml webapps/manager/META-INF/context.xml"
-
-#docker exec -it "$(docker container ls | grep 'issuebase-server_tomcat_1' | awk '{print $1}')" sh -c "cd /issuebase-server/oauth && mvn tomcat7:deploy"
+if [ "$1" == "-t" ]
+then
+    docker exec -it "$(docker container ls | grep 'issuebase-server_tomcat_1' | awk '{print $1}')" sh -c "mv webapps webapps2 && mv webapps.dist/ webapps"
+    docker exec -it "$(docker container ls | grep 'issuebase-server_tomcat_1' | awk '{print $1}')" sh -c "rm webapps/manager/META-INF/context.xml && cp context.xml webapps/manager/META-INF/context.xml"
+    docker exec -it "$(docker container ls | grep 'issuebase-server_tomcat_1' | awk '{print $1}')" sh -c "cd /issuebase-server/oauth && mvn tomcat7:deploy"
+fi
