af4514f32df74a5ce574a24bb9d2e881752f4f69
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-11-20 12:16:44 +0530

    support wsl

--- up.sh
+++ up.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-docker-compose up -d
+
+if [ "$1" == "-t" ]
+then
+    docker-compose -f docker-compose.yaml up -d
+else
+    docker-compose -f docker-compose-standard.yaml up -d
+fi
+
+if grep -q Microsoft /proc/version; then
+  name=issuebase-server-db-1
+else
+  name=issuebase-server_db_1
+fi
 
 sleep 5s
 
-mysql_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' issuebase-server-db-1)
+mysql_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $name)
 
 connection_string="mysql -h $mysql_ip -u issuebase -pissuebase issuebase < /up.sql"
 
-docker exec -it "$(docker container ls  | grep 'issuebase-server-db-1' | awk '{print $1}')" sh -c "$connection_string"
+docker exec -it "$(docker container ls  | grep $name | awk '{print $1}')" sh -c "$connection_string"
 
-echo issuebase-server-db-1 "$mysql_ip"
+echo $name "$mysql_ip"
 
-echo issuebase-server-redis-1 "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' issuebase-server-db-1)"
+echo issuebase-server-redis-1 "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $name)"
 
 if [ "$1" == "-t" ]
 then
